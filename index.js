c486878bc601714e62357ffdb8d333716d4bbe83
Hashir13 <hasanhashir1314@gmail.com>
2019-09-22 18:17:18 +0530

    updated

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const request=require('request-promise')
 const cheerio=require('cheerio')
 const regularrequest=require('request')
 const fs=require('fs')
-// const Nightmare=require('nightmare')
-
 
 const sample_data={
     FullName:"Shah Rukh Khan",
@@ -33,10 +31,18 @@ if(user.is_private)
 else{
     user.is_private ="Public";
 }
+if(user.is_verified)
+{
+    user.is_verified ="Verified";
+}
+else{
+    user.is_verified ="Not Verified";
+}
 let user_data={
     Account:user.is_private,
+    Public_Figure:user.is_verified,
     FullName:user.full_name,
-      Total_Posts:user.edge_owner_to_timeline_media.count,
+    Total_Posts:user.edge_owner_to_timeline_media.count,
     Followers:user.edge_followed_by.count,
     Following:user.edge_follow.count,
     Profile_Pic_Url_Low:user.profile_pic_url,
@@ -54,8 +60,8 @@ console.log("\nIMAGES(Low and Hd Quality both) ARE DOWNLOADED TO THE 'images' FO
 
 async function saveimagetodisk(user)
 {
-    regularrequest.get(user.profile_pic_url).pipe(fs.createWriteStream(`images/${user.username}_low.png`))
-    regularrequest.get(user.profile_pic_url_hd).pipe(fs.createWriteStream(`images/${user.username}_hd.png`))
+    regularrequest.get(user.profile_pic_url).pipe(fs.createWriteStream(`Insta Profile Images/${user.username}_low.png`))
+    regularrequest.get(user.profile_pic_url_hd).pipe(fs.createWriteStream(`Insta Profile Images/${user.username}_hd.png`))
 }
 
 
